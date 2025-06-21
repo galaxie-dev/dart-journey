028ab89f88a6b0d0ef1f3737643c510af9b52af8
Evans Osumba <osumbaevanzz@gmail.com>
2025-06-21 22:36:47 +0300

    changes hapa na pale

--- map.dart
+++ map.dart
@@ -1,2 +1,30 @@
 //Map - collection of key value keys where each value is unique
 
+
+import 'OOP/abstraction.dart';
+
+void main() {
+  final list = [20,30,40];
+
+  Map<String, int> marks =  {
+    "Evans" : 20,
+    "Jack" : 30,
+    "Steve" : 40,
+  };
+
+// print(marks["Evans"]);
+
+//adds a value
+marks["David"] = 40;
+
+//updates a value
+marks["Evans"] = 90;
+print(marks);
+
+
+// if(marks["Evans"] == null) {
+//   print("Key does not exist");
+// } else{
+//   print(marks["Naman"]!.isEven);
+// }
+}
